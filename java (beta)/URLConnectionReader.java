164d6d41700a6c56628dc0cd2eec93f49adce737
Arnaud Aliès <arnaudalies.py@gmail.com>
2015-04-21 15:49:05 +0200

    Fixed bug

--- java (beta)/URLConnectionReader.java
+++ java (beta)/URLConnectionReader.java
@@ -5,8 +5,14 @@ import java.net.URLConnection;
 
 public class URLConnectionReader {
     public static String getText(String url) throws Exception {
-        URL website = new URL(url);
-        URLConnection connection = website.openConnection();
+        //String charset = java.nio.charset.StandardCharsets.UTF_8.name();//"UTF-8"
+        String charset = "UTF-8";
+        
+        URLConnection connection = new URL(url).openConnection();
+        
+		connection.setRequestProperty("Accept-Charset", charset);
+        connection.addRequestProperty("User-Agent", "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; .NET CLR 1.1.4322; .NET CLR 2.0.50727; .NET CLR 3.0.04506.30)");
+        
         BufferedReader in = new BufferedReader(
                                 new InputStreamReader(
                                     connection.getInputStream()));
