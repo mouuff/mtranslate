1b6443c3f63574a3e432b4f8331360516ac7de3b
Arnaud Aliès <mouu@hush.com>
2015-04-21 16:31:32 +0200

    indentation issues

--- java/URLConnectionReader.java
+++ java/URLConnectionReader.java
@@ -10,7 +10,7 @@ public class URLConnectionReader {
         
         URLConnection connection = new URL(url).openConnection();
         
-		connection.setRequestProperty("Accept-Charset", charset);
+        connection.setRequestProperty("Accept-Charset", charset);
         connection.addRequestProperty("User-Agent", "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; .NET CLR 1.1.4322; .NET CLR 2.0.50727; .NET CLR 3.0.04506.30)");
         
         BufferedReader in = new BufferedReader(
--- java/translate.java
+++ java/translate.java
@@ -2,39 +2,39 @@ import java.net.*;
 import java.util.*;
 
 public class translate {
-	
-	public static String translate(String to_translate, String to_langage, String from_langage){
-		String page, result, hl, sl, q;
-		
-		String before_trans = "class=\"t0\">";
-		
-		//String charset = java.nio.charset.StandardCharsets.UTF_8.name();
-		String charset = "UTF-8";
-		
-		try{
-			hl = URLEncoder.encode(to_langage, charset);
-			sl = URLEncoder.encode(from_langage, charset);
-			q = URLEncoder.encode(to_translate, charset);
-		}catch(Exception e){
-			e.printStackTrace();
-			return "";
-		}
-		
-		String query = String.format("https://translate.google.com/m?hl=%s&sl=%s&q=%s", hl,sl,q);
-		
-		try {
-			page = URLConnectionReader.getText(query);
-		} catch (Exception e) {
-			e.printStackTrace();
-			return "";
-		}
-		result = page.substring(page.indexOf(before_trans)+before_trans.length());
-		result = result.split("<")[0];
-		return result;
-	}
-	public static void main(String[] args){
-		String text = "Salut toi";
-		System.out.println(text+" >> "+translate(text,"en","auto"));//simple example to see if it works
-		System.out.println(text+" >> "+translate(text,"es","auto"));
-	}
+    
+    public static String translate(String to_translate, String to_langage, String from_langage){
+        String page, result, hl, sl, q;
+        
+        String before_trans = "class=\"t0\">";
+        
+        //String charset = java.nio.charset.StandardCharsets.UTF_8.name();
+        String charset = "UTF-8";
+        
+        try{
+            hl = URLEncoder.encode(to_langage, charset);
+            sl = URLEncoder.encode(from_langage, charset);
+            q = URLEncoder.encode(to_translate, charset);
+        }catch(Exception e){
+            e.printStackTrace();
+            return "";
+        }
+        
+        String query = String.format("https://translate.google.com/m?hl=%s&sl=%s&q=%s", hl,sl,q);
+        
+        try {
+            page = URLConnectionReader.getText(query);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return "";
+        }
+        result = page.substring(page.indexOf(before_trans)+before_trans.length());
+        result = result.split("<")[0];
+        return result;
+    }
+    public static void main(String[] args){
+        String text = "Salut toi";
+        System.out.println(text+" >> "+translate(text,"en","auto"));//simple example to see if it works
+        System.out.println(text+" >> "+translate(text,"es","auto"));
+    }
 }
