dc18f18b2b09ba38c9707a7d786be0e24b4978b7
alies_a <arnaud.alies@epitech.eu>
2016-09-14 14:54:50 +0200

    not using six

--- python/translate.py
+++ python/translate.py
@@ -1,16 +1,15 @@
 #!/usr/bin/env python
 # encoding: utf-8
 
-import six
+import sys
+import re
 
-if (six.PY2):
+if (sys.version_info[0] < 3):
 	import urllib2
-	import re
 	import urllib
 else:
 	import urllib.request
 	import urllib.parse
-	import re
 
 agent = {'User-Agent' : "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; .NET CLR 1.1.4322; .NET CLR 2.0.50727; .NET CLR 3.0.04506.30)"}
 
@@ -18,19 +17,23 @@ agent = {'User-Agent' : "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1;
 def translate(to_translate, to_language="auto", language="auto"):
 	"""
 	Returns the translation using google translate
-	you must shortcut the language you define (French = fr, English = en, Spanish = es, etc...)
-	if you don't define anything it will detect it or use english by default
+	you must shortcut the language you define
+	(French = fr, English = en, Spanish = es, etc...)
+	if not defined it will detect it or use english by default
+	
 	Example:
 	print(translate("salut tu vas bien?", "en"))
 	hello you alright?
 	"""
 	base_link = "http://translate.google.com/m?hl=%s&sl=%s&q=%s"
-	if (six.PY2):
-		link = base_link % (to_language, language, urllib.pathname2url(to_translate))
+	if (sys.version_info[0] < 3):
+		to_translate = urllib.pathname2url(to_translate)
+		link = base_link % (to_language, language, to_translate)
 		request = urllib2.Request(link, headers=agent)
 		page = urllib2.urlopen(request).read()
 	else:
-		link = base_link % (to_language, language, urllib.parse.quote(to_translate))
+		to_translate = urllib.parse.quote(to_translate)
+		link = base_link % (to_language, language, to_translate)
 		request = urllib.request.Request(link, headers=agent)
 		page = urllib.request.urlopen(request).read().decode("utf-8")
 	expr = r'class="t0">(.*?)<'
