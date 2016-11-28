8eefde2fb01b1f21d7b699680b3f843ae3be33db
alies_a <arnaud.alies@epitech.eu>
2016-11-28 09:59:33 +0100

    escaping html

--- mtranslate/core.py
+++ mtranslate/core.py
@@ -31,20 +31,30 @@ import re
 if (sys.version_info[0] < 3):
     import urllib2
     import urllib
+    import HTMLParser
 else:
+    import html.parser
     import urllib.request
     import urllib.parse
 
 agent = {'User-Agent':
-    "Mozilla/4.0 (\
-    compatible;\
-    MSIE 6.0;\
-    Windows NT 5.1;\
-    SV1;\
-    .NET CLR 1.1.4322;\
-    .NET CLR 2.0.50727;\
-    .NET CLR 3.0.04506.30\
-    )"}
+"Mozilla/4.0 (\
+compatible;\
+MSIE 6.0;\
+Windows NT 5.1;\
+SV1;\
+.NET CLR 1.1.4322;\
+.NET CLR 2.0.50727;\
+.NET CLR 3.0.04506.30\
+)"}
+
+
+def unescape(text):
+    if (sys.version_info[0] < 3):
+        parser = HTMLParser.HTMLParser()
+    else:
+        parser = html.parser.HTMLParser()
+    return (parser.unescape(text))
 
 
 def translate(to_translate, to_language="auto", from_language="auto"):
@@ -69,7 +79,9 @@ def translate(to_translate, to_language="auto", from_language="auto"):
         request = urllib.request.Request(link, headers=agent)
         page = urllib.request.urlopen(request).read().decode("utf-8")
     expr = r'class="t0">(.*?)<'
-    result = re.findall(expr, page)
-    if (len(result) == 0):
-        return ("")
-    return(result[0])
+    re_result = re.findall(expr, page)
+    if (len(re_result) == 0):
+        result = ""
+    else:
+        result = unescape(re_result[0])
+    return (result)
--- setup.py
+++ setup.py
@@ -4,12 +4,12 @@ from distutils.core import setup
 setup(
     name = 'mtranslate',
     packages = ['mtranslate'],
-    version = '1.3',
+    version = '1.4',
     description = 'Google translate console script with easy to use API',
     author = 'Arnaud Alies',
     author_email = 'arnaudalies.py@gmail.com',
     url = 'https://github.com/mouuff/mtranslate',
-    download_url = 'https://github.com/mouuff/mtranslate/tarball/1.3',
+    download_url = 'https://github.com/mouuff/mtranslate/tarball/1.4',
     keywords = ['console', 'translate', 'translator', 'simple', 'google'],
     classifiers = [],
     entry_points={
