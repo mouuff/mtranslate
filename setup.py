6b2421fcbdfdaacc8969c4d0b5086cd6f87051dc
Arnaud Alies <arnaudalies.py@gmail.com>
2020-11-21 11:17:53 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,19 +2,20 @@
 from distutils.core import setup
 
 setup(
-    name = 'mtranslate',
-    packages = ['mtranslate'],
-    version = '1.6',
-    description = 'Google translate console script with easy to use API',
-    author = 'Arnaud Alies',
-    author_email = 'arnaudalies.py@gmail.com',
-    url = 'https://github.com/mouuff/mtranslate',
-    download_url = 'https://github.com/mouuff/mtranslate/tarball/1.6',
-    keywords = ['console', 'translate', 'translator', 'simple', 'google', 'language'],
-    classifiers = [],
+    name='mtranslate',
+    packages=['mtranslate'],
+    version='1.7',
+    description='Google translate console script with easy to use API',
+    author='Arnaud Alies',
+    author_email='arnaudalies.py@gmail.com',
+    url='https://github.com/mouuff/mtranslate',
+    download_url='https://github.com/mouuff/mtranslate/tarball/1.6',
+    keywords=['console', 'translate', 'translator',
+              'simple', 'google', 'language'],
+    classifiers=[],
     entry_points={
-          'console_scripts': [
-              'mtranslate = mtranslate.__main__:main'
-          ]
-      },
+        'console_scripts': [
+            'mtranslate = mtranslate.__main__:main'
+        ]
+    },
 )
